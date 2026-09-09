e89255c4b36e8dae568ad9cf9bf9841cee513b04
unknown <zhenglangChen@outlook.com>
2026-09-09 20:11:25 +0800

    C++ OpenCV

--- C++_study/Opencv/main.cpp
+++ C++_study/Opencv/main.cpp
@@ -1,26 +1,62 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+using namespace std;
 
 int main()
 {
-    cv::Mat image =
-        cv::Mat::zeros(480, 640, CV_8UC3);
+    // 读取图片
+    cv::Mat image = cv::imread("test.png");
 
-    cv::rectangle(
+    if (image.empty())
+    {
+        cout << "Fail to load image" << endl;
+        return -1;
+    }
+
+    // image.shape
+    cout << "width:" << image.cols << endl;
+    cout << "heigh:" << image.rows << endl;
+    cout << "channels:" << image.channels() << endl;
+
+    // Resize
+    cv::Mat resized;
+    cv::resize(
         image,
-        cv::Point(100, 100),
-        cv::Point(300, 300),
-        cv::Scalar(0, 255, 0),
-        2);
+        resized,
+        cv::Size(640, 640));
 
-    cv::imwrite("opencv_test.jpg", image);
+    // cv.BGR2RGB
+    cv::Mat rgb;
+    cv::cvtColor(
+        resized,
+        rgb,
+        cv::COLOR_BGR2RGB);
 
-    std::cout
-        << "Image shape: "
-        << image.cols
-        << " x "
-        << image.rows
-        << std::endl;
+    cv::imwrite("output.jpg", resized);
+    cout << "Done!" << endl;
 
-    return 0;
+    // 读取视频
+    cv::VideoCapture cap("test.mp4");
+    if (!cap.isOpened())
+    {
+        cout << "Fail to cap the video!" << endl;
+        return -1;
+    }
+
+    cout << "FPS:" << cap.get(cv::CAP_PROP_FPS) << endl;
+    cout << "width:" << cap.get(cv::CAP_PROP_FRAME_WIDTH) << endl;
+    cout << "heigh:" << cap.get(cv::CAP_PROP_FRAME_HEIGHT) << endl;
+
+    cv::Mat frame;
+    int frame_id = 0;
+    while (true)
+    {
+        if (!cap.read(frame))
+        {
+            break;
+        }
+        frame_id++;
+    }
+
+    cout << "success read frame:" << frame_id << endl;
 }
